42f9153ef99d98777fb41ee43e5a3a0a78deb8af
Kawarime <kaliak.miroslav@gmail.com>
2024-06-21 15:27:56 +0300

    але пулься

--- LMS/urls.py
+++ LMS/urls.py
@@ -3,6 +3,7 @@ from LMS.views import *
 
 urlpatterns = [
     path('', CourseView.as_view(), name = "main"),
+    path('<int:pk>/', TaskListView.as_view(), name = "task_list"),
 ]
 
 app_name = "lms"
