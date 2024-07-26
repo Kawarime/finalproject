166e00922351b9870f19f28045a5ad8c6d22bea8
kawarime <kaliak.miroslav@gmail.com>
2024-07-26 15:21:15 +0300

    media

--- LMS/models.py
+++ LMS/models.py
@@ -85,8 +85,10 @@ class CustomUser(AbstractUser):
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    short_description = models.CharField(max_length=100,null=True, blank=True)
+    description = models.CharField(max_length=100,null=True, blank=True)
+    image = models.FileField(upload_to="course_media", blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
 
     def __str__(self):
         return f"{self.name}"
